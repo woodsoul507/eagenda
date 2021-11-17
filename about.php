719a4d40b20da53f9e2ebda1ab6a4b80b248a145
woodsoul507 <josealfu@gmail.com>
2021-11-16 23:54:43 -0500

    first commit

---
+++ about.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EAgenda - Home</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@1.16.2/dist/full.css" rel="stylesheet" type="text/css" />
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2/dist/tailwind.min.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+
+    <div class="navbar mb-2 shadow-lg bg-neutral text-neutral-content">
+        <div class="px-2 mx-2 navbar-start">
+            <span class="text-lg font-bold">
+                EAgenda
+            </span>
+        </div>
+        <div class="hidden px-2 mx-2 navbar-center lg:flex">
+            <div class="flex items-stretch">
+                <a href="index.php" class="btn btn-ghost btn-sm rounded-btn">
+                    Home
+                </a>
+                <a href="create.php" class="btn btn-ghost btn-sm rounded-btn">
+                    Create
+                </a>
+                <a href="about.php" class="btn btn-ghost btn-sm rounded-btn">
+                    About
+                </a>
+            </div>
+        </div>
+        <div class="navbar-end">
+
+        </div>
+    </div>
+    <h2 class="flex justify-center mt-6  text-3xl">About</h2>
+    <div class="card text-center shadow-2xl">
+
+        <div class="card-body">
+            <h2 class="card-title">¿Qué es EAgenda?</h2>
+            <p>EAgenda es el inicio de un proyecto que busca crear la agenda electrónica más completa y con la mayor cantidad de funcionalidades que permite la tecnología actual.
+                <br>EAgenda es además un proyecto universitario y libre.
+            </p>
+            <div class="justify-center card-actions">
+
+            </div>
+        </div>
+    </div>
+
+</body>
+
+<footer class="p-10 footer bg-base-200 text-base-content footer-center">
+    <div class="grid grid-flow-col gap-4">
+        <a href="index.php" class="link link-hover">Home</a>
+
+    </div>
+
+    <div>
+        <p>Copyright © 2021 - All right reserved by José Alfú</p>
+    </div>
+</footer>
+
+</html>
---
+++ database.php
@@ -0,0 +1,93 @@
+<?php
+
+class Database
+{
+    private $con;
+    private string $dbhost = $_SERVER['EA_HOST'];
+    private string $dbuser = $_SERVER['EA_USER'];
+    private string $dbpass = $_SERVER['EA_PASS'];
+    private string $dbname = $_SERVER['EA_DB'];
+    function __construct()
+    {
+        $this->connect_db();
+    }
+    public function connect_db()
+    {
+        $this->con = mysqli_connect($this->dbhost, $this->dbuser, $this->dbpass, $this->dbname);
+        if (mysqli_connect_error()) {
+            die("Server can not connect to database " . mysqli_connect_error() . mysqli_connect_errno());
+        }
+    }
+
+    public function sanitize($var)
+    {
+        $return = mysqli_real_escape_string($this->con, $var);
+        return $return;
+    }
+
+    public function create(
+        $contact_nickname,
+        $contact_name,
+        $contact_lastname,
+        $contact_number,
+        $contact_email,
+        $contact_address,
+        $contact_image
+    ) {
+        $sql = "INSERT INTO `contacts` (create_time, contact_nickname, contact_name, contact_lastname, contact_number, contact_email, contact_address, contact_image) VALUES (NOW(),
+        '$contact_nickname', '$contact_name', '$contact_lastname', '$contact_number', '$contact_email', '$contact_address', '$contact_image')";
+        $res = mysqli_query($this->con, $sql);
+        if ($res) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function read()
+    {
+        $sql = "SELECT * FROM contacts";
+        $res = mysqli_query($this->con, $sql);
+        return $res;
+    }
+
+    public function single_record($contact_id)
+    {
+        $sql = "SELECT * FROM contacts where contact_id='$contact_id'";
+        $res = mysqli_query($this->con, $sql);
+        $return = mysqli_fetch_object($res);
+        return $return;
+    }
+
+    public function update(
+        $contact_nickname,
+        $contact_name,
+        $contact_lastname,
+        $contact_number,
+        $contact_email,
+        $contact_address,
+        $contact_image,
+        $contact_id
+    ) {
+        $sql = "UPDATE contacts SET update_time=NOW(), contact_nickname='$contact_nickname', contact_name='$contact_name',
+        contact_lastname='$contact_lastname', contact_number='$contact_number', contact_email='$contact_email',
+        contact_address='$contact_address', contact_image='$contact_image' WHERE contact_id=$contact_id";
+        $res = mysqli_query($this->con, $sql);
+        if ($res) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function delete($contact_id)
+    {
+        $sql = "DELETE FROM contacts WHERE contact_id=$contact_id";
+        $res = mysqli_query($this->con, $sql);
+        if ($res) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
