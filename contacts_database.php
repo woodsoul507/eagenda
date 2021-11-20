70f7e3519616bf4df288ac87ed4a9b7902cecd20
Jose Alfu <josealfu@gmail.com>
2021-11-20 15:08:07 -0500

    Dotenv added

--- contacts_database.php
+++ contacts_database.php
@@ -1,12 +1,22 @@
 <?php
 
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('HOST', $_ENV['DB_HOST']);
+define('USER', $_ENV['DB_USER']);
+define('PASS', $_ENV['DB_PASS']);
+define('NAME', $_ENV['DB_NAME']);
+
 class ContactsDatabase
 {
     private $con;
-    private string $dbhost = "localhost";
-    private string $dbuser = "root";
-    private string $dbpass = "";
-    private string $dbname = "directorio_personal";
+    private string $dbhost = HOST;
+    private string $dbuser = USER;
+    private string $dbpass = PASS;
+    private string $dbname = NAME;
     function __construct()
     {
         $this->connect_db();
@@ -27,15 +37,15 @@ class ContactsDatabase
 
     public function create(
         $contact_nickname,
-        $contact_name,
+        $contact_firstname,
         $contact_lastname,
         $contact_number,
         $contact_email,
         $contact_address,
         $contact_image
     ) {
-        $sql = "INSERT INTO `contacts` (create_time, contact_nickname, contact_name, contact_lastname, contact_number, contact_email, contact_address, contact_image) VALUES (NOW(),
-        '$contact_nickname', '$contact_name', '$contact_lastname', '$contact_number', '$contact_email', '$contact_address', '$contact_image')";
+        $sql = "INSERT INTO `contacts` (create_time, contact_nickname, contact_firstname, contact_lastname, contact_number, contact_email, contact_address, contact_image) VALUES (NOW(),
+        '$contact_nickname', '$contact_firstname', '$contact_lastname', '$contact_number', '$contact_email', '$contact_address', '$contact_image')";
         $res = mysqli_query($this->con, $sql);
         if ($res) {
             return true;
@@ -61,7 +71,7 @@ class ContactsDatabase
 
     public function update(
         $contact_nickname,
-        $contact_name,
+        $contact_firstname,
         $contact_lastname,
         $contact_number,
         $contact_email,
@@ -69,7 +79,7 @@ class ContactsDatabase
         $contact_image,
         $contact_id
     ) {
-        $sql = "UPDATE contacts SET update_time=NOW(), contact_nickname='$contact_nickname', contact_name='$contact_name',
+        $sql = "UPDATE contacts SET update_time=NOW(), contact_nickname='$contact_nickname', contact_firstname='$contact_firstname',
         contact_lastname='$contact_lastname', contact_number='$contact_number', contact_email='$contact_email',
         contact_address='$contact_address', contact_image='$contact_image' WHERE contact_id=$contact_id";
         $res = mysqli_query($this->con, $sql);
--- users_database.php
+++ users_database.php
@@ -1,12 +1,22 @@
 <?php
 
-class UsersDatabase
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+use Dotenv\Dotenv;
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+define('HOST', $_ENV['DB_HOST']);
+define('USER', $_ENV['DB_USER']);
+define('PASS', $_ENV['DB_PASS']);
+define('NAME', $_ENV['DB_NAME']);
+
+class ContactsDatabase
 {
     private $con;
-    private string $dbhost = "localhost";
-    private string $dbuser = "root";
-    private string $dbpass = "";
-    private string $dbname = "directorio_personal";
+    private string $dbhost = HOST;
+    private string $dbuser = USER;
+    private string $dbpass = PASS;
+    private string $dbname = NAME;
     function __construct()
     {
         $this->connect_db();
@@ -27,7 +37,7 @@ class UsersDatabase
 
     public function create(
         $user_nickname,
-        $user_username,
+        $user_firstname,
         $user_lastname,
         $user_password,
         $user_birthday,
@@ -36,8 +46,8 @@ class UsersDatabase
         $user_address,
         $user_image
     ) {
-        $sql = "INSERT INTO `users` (create_time, user_nickname, user_username, user_lastname, user_password, user_birthday, user_number, user_email, user_address, user_image) VALUES (NOW(),
-        '$user_nickname', '$user_username', '$user_lastname', '$user_password', '$user_birthday', '$user_number', '$user_email', '$user_address', '$user_image')";
+        $sql = "INSERT INTO `users` (create_time, user_nickname, user_firstname, user_lastname, user_password, user_birthday, user_number, user_email, user_address, user_image) VALUES (NOW(),
+        '$user_nickname', '$user_firstname', '$user_lastname', '$user_password', '$user_birthday', '$user_number', '$user_email', '$user_address', '$user_image')";
         $res = mysqli_query($this->con, $sql);
         if ($res) {
             return true;
@@ -63,7 +73,7 @@ class UsersDatabase
 
     public function update(
         $user_nickname,
-        $user_username,
+        $user_firstname,
         $user_lastname,
         $user_password,
         $user_birthday,
@@ -73,7 +83,7 @@ class UsersDatabase
         $user_image,
         $user_id
     ) {
-        $sql = "UPDATE users SET update_time=NOW(), user_nickname='$user_nickname', user_username='$user_username',
+        $sql = "UPDATE users SET update_time=NOW(), user_nickname='$user_nickname', user_firstname='$user_firstname',
         user_lastname='$user_lastname', user_password='$user_password', user_birthday='$user_birthday',
         user_number='$user_number', user_email='$user_email', user_address='$user_address', user_image='$user_image'
         WHERE user_id=$user_id";
